c158c359a8e5baa0c7aa39870961685116464923
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-02-12 01:40:35 +0100

    .........

---
+++ projet2/src/Twig/CatsExtension.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Twig;
+
+
+use App\Entity\Newsletters\Users;
+use App\Form\NewslettersUsersFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CatsExtension extends AbstractController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('cats', [$this, 'getFooter'])
+        ];
+    }
+
+
+
+    public function getFooter(Request $request,EntityManagerInterface $em, MailerInterface $mailer)
+    {
+        $user = new Users();
+    $form = $this->createForm(NewslettersUsersFormType::class, $user);
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()){
+        $token = hash('sha256', uniqid());
+
+        $user->setValidationToken($token);
+
+        
+        $em->persist($user);
+        $em->flush();
+
+        $email = (new TemplatedEmail())
+            ->from('newsletter@site.fr')
+            ->to($user->getEmail())
+            ->subject('Votre inscription à la newsletter')
+            ->htmlTemplate('emails/inscription.html.twig')
+            ->context(compact('user', 'token'))
+            ;
+
+        $mailer->send($email);
+
+
+ 
+        return $this->redirectToRoute('newsletters_list');
+    }
+
+    
+    return $this->render('newsletters/prepare.html.twig', [
+        'form' => $form->createView()
+    ]);
+       
+    
+    }
+}
